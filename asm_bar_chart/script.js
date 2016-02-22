07a9aeeda7a28de31c9099d7389e0f857d560cb9
Andy <andrew.sunglae.kim@gmail.com>
2016-02-22 15:00:26 -0500

    adds asm chart

---
+++ asm_bar_chart/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+  var states = ["Alabama", "Alaska", "Arizona", "Arkansas"]
+  var numEmployees = [234726, 12933, 134860, 148672]
+  var ctx = $("#myChart").get(0).getContext("2d");
+  var data = {
+    labels: states,
+    datasets: [
+      {
+        label: "Number Of Employees",
+        fillColor: "lemonchiffon",
+        strokeColor: "black",
+        data: numEmployees
+      }
+    ]
+  }
+
+  var myBarChart = new Chart(ctx).Bar(data, {
+    multiTooltipTemplate: "<%= datasetLabel %> - <%= value %>"
+  })
+})
